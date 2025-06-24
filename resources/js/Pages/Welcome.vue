1c200a1ccfed09c0cb8a86b187cc8ff35e9dfcf0
Diego Vargas <diego@yafun.cl>
2025-06-24 13:21:26 +0100

    initial setup

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,85 @@
+<script setup>
+import { Head, Link } from "@inertiajs/vue3";
+import { Icon } from "@iconify/vue";
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+});
+</script>
+
+<template>
+    <Head title="Welcome" />
+    <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
+        <img id="background" class="absolute -left-20 top-0 max-w-[877px]" />
+        <div
+            class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white"
+        >
+            <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
+                <header
+                    class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3"
+                >
+                    <div class="flex lg:justify-center lg:col-start-2"></div>
+                    <nav v-if="canLogin" class="-mx-3 flex flex-1 justify-end">
+                        <Link
+                            v-if="$page.props.auth.user"
+                            :href="route('dashboard')"
+                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-hidden focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                        >
+                            Dashboard
+                        </Link>
+
+                        <template v-else>
+                            <Link
+                                :href="route('login')"
+                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-hidden focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                            >
+                                Log in
+                            </Link>
+
+                            <Link
+                                v-if="canRegister"
+                                :href="route('register')"
+                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-hidden focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                            >
+                                Register
+                            </Link>
+                        </template>
+                    </nav>
+                </header>
+
+                <main class="mt-6">
+                    <!-- The button to open modal -->
+                    <label for="my_modal_7" class="btn">
+                        <Icon
+                            icon="line-md:briefcase-check"
+                            class="w-10 h-auto"
+                        />
+                    </label>
+
+                    <!-- Put this part before </body> tag -->
+                    <input
+                        type="checkbox"
+                        id="my_modal_7"
+                        class="modal-toggle"
+                    />
+                    <div class="modal" role="dialog">
+                        <div class="modal-box">
+                            <h3 class="text-lg font-bold">Hello!</h3>
+                            <p class="py-4">
+                                This modal works with a hidden checkbox!
+                            </p>
+                        </div>
+                        <label class="modal-backdrop" for="my_modal_7"
+                            >Close</label
+                        >
+                    </div>
+                </main>
+            </div>
+        </div>
+    </div>
+</template>
